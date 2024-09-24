75964513403838d399feaca278c37ebf14b276e7
Yazan Haddad <127855239+hxddad@users.noreply.github.com>
2024-09-23 21:43:25 -0400

    Update main.py

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
 REDDIT_CLIENT_ID = os.getenv('REDDIT_CLIENT_ID')
 REDDIT_CLIENT_SECRET = os.getenv('REDDIT_CLIENT_SECRET')
 REDDIT_USER_AGENT = os.getenv('REDDIT_USER_AGENT')
+USER_ID = os.getenv('USER_ID')
+
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -20,24 +22,23 @@ reddit = praw.Reddit(client_id=REDDIT_CLIENT_ID,
                      client_secret=REDDIT_CLIENT_SECRET,
                      user_agent=REDDIT_USER_AGENT)
 
-CHANNEL_ID = 1268267896547709031  # Channel ID
-SUBREDDIT = 'HonkaiStarRail_Leaks' # Subreddit Name
+SUBREDDIT = 'HonkaiStarRail_Leaks'
 
 async def fetch_new_reddit_posts(subreddit, last_post_id=None):
     subreddit = reddit.subreddit(subreddit)
     posts = []
-    for submission in subreddit.new(limit=1):  # Fetch 1 post 
-        if last_post_id and submission.id == last_post_id: # If post is already sent, ignore
+    for submission in subreddit.new(limit=1):  # Fetch up to 1 new post
+        if last_post_id and submission.id == last_post_id:
             break
         message = f"**{submission.title}**\nhttps://reddit.com{submission.permalink}"
-        posts.append((submission.id, message)) # Else, add link and post to posts array
+        posts.append((submission.id, message))
     return posts
 
 async def check_for_new_posts():
     await client.wait_until_ready()
-    channel = client.get_channel(CHANNEL_ID)
-    if channel is None:
-        print(f"Channel with ID {CHANNEL_ID} not found")
+    user = await client.fetch_user(USER_ID)  # Fetch the user by their ID
+    if user is None:
+        print(f"User with ID {USER_ID} not found")
         return
     last_post_id = None
 
@@ -47,7 +48,7 @@ async def check_for_new_posts():
             if new_posts:
                 last_post_id = new_posts[0][0]  # Update last_post_id to the most recent post
                 for post_id, post_content in reversed(new_posts):
-                    await channel.send(post_content)
+                    await user.send(post_content)  # Send the post to the user's DM
         except Exception as e:
             print(f"Error fetching or sending posts: {e}")
         await asyncio.sleep(10)  # Check every 10 seconds
