cb2461b1949459a8fb8e87ac393ddb476fe95b79
hxddad <haddad.yazan117@gmail.com>
2024-09-24 20:21:01 -0400

    fixed classname

--- hello-cdk/lib/hello-cdk-stack.ts
+++ hello-cdk/lib/hello-cdk-stack.ts
@@ -2,7 +2,7 @@ import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 import * as lambda from "aws-cdk-lib/aws-lambda";
 
-export class DiscordBotLambdaStack extends cdk.Stack {
+export class HelloCdkStack extends cdk.Stack {  // Changed class name here
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
