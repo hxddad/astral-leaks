ed32b82c7862d76381b0b88ec60ebe5fdb57c4f9
hxddad <haddad.yazan117@gmail.com>
2024-09-24 20:06:12 -0400

    added aws cdk

---
+++ hello-cdk/lib/hello-cdk-stack.ts
@@ -0,0 +1,36 @@
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+
+export class DiscordBotLambdaStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const dockerFunction = new lambda.DockerImageFunction(
+      this,
+      "DockerFunction",
+      {
+        code: lambda.DockerImageCode.fromImageAsset("./src"),
+        memorySize: 1024,
+        timeout: cdk.Duration.seconds(10),
+        architecture: lambda.Architecture.ARM_64,
+        environment: {
+          DISCORD_PUBLIC_KEY: "4454ed3bbb90656a17f876fefd1a676e29d46169c5308b5e4db69c4ec1751f90",
+        },
+      }
+    );
+
+    const functionUrl = dockerFunction.addFunctionUrl({
+      authType: lambda.FunctionUrlAuthType.NONE,
+      cors: {
+        allowedOrigins: ["*"],
+        allowedMethods: [lambda.HttpMethod.ALL],
+        allowedHeaders: ["*"],
+      },
+    });
+
+    new cdk.CfnOutput(this, "FunctionUrl", {
+      value: functionUrl.url,
+    });
+  }
+}
